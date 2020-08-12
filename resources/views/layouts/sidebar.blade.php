f8d779b6d7368f111b9e4a22a4d880f769cd8511
Andhika277 <raysteiner416@gmail.com>
2020-08-12 12:58:56 +0700

    pertemuan 10

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,8 @@
+<h4> Main Menu</h4>
+<div class="list-group list-group-flush">
+  <a href="/mhs" class="list-group-item list-group-item-action">Mahasiswa</a>
+  <a href="#" class="list-group-item list-group-item-action">Dosen</a>
+  <a href="/prodi" class="list-group-item list-group-item-action">Prodi</a>
+  <a href="#" class="list-group-item list-group-item-action">Kerja Praktek</a>
+  <a href="#" class="list-group-item list-group-item-action">Skripsi</a>
+</div>
