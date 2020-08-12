3e3a52e4f1aa73e38aa7f8a6c549c6f0e6cc88af
Andhika277 <raysteiner416@gmail.com>
2020-08-12 13:18:02 +0700

    pertemuan 13: uas fitur prodi

--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
-@section('title','Prodi Page')
+@section('title','Halaman Prodi')
 @section('bread1','Prodi')
 @section('bread2','Data')
 @section('content')
     <h3>Master Data Prodi</h3>
-    <p><a href="/prodi/create" class="btn btn-success btn-sm">Tambah</a></p>
+    <p><a href="/prodi/create" class="btn btn-success btn-sm">Tambah Data</a></p>
 
     @include('layouts.alert')
     
